409e70e4761ed786956962b82469645b9581ff94
Rymney34 <104559353+Rymney34@users.noreply.github.com>
2025-07-11 01:09:16 +0100

    sorrting header and adding new staff, and checking react,git on onther device

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -9,9 +9,11 @@ class AppHeader extends Component {
 
         return (
                 <div className="header">
-                    <nav>
-                         <img src="" alt="logo" />
-                        <ul>
+                    
+                    <nav className='head-nav'>
+                   
+                        <ul className='list-nav'>
+                        <img src="" alt="logo" />
                             <li><a href="#">Coffee House</a></li>
                             <li><a href="#">Our coffee</a></li>
                             <li><a href="#">For your pleasure</a></li>
