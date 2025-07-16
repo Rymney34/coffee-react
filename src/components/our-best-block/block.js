ace8ad3eeb4415e429504a05e17a2fd461d39bc6
Rymney34 <104559353+Rymney34@users.noreply.github.com>
2025-07-16 23:57:31 +0100

    Text centered our best and div for box is started

---
+++ src/components/our-best-block/block.js
@@ -0,0 +1,28 @@
+import './our-best-block.css' 
+
+
+import {Component} from 'react'
+
+class itemBlock extends Component {
+
+ 
+    render () {
+
+        return (
+            
+                 <div className="block-bx">
+                    <img className="bg-image"  alt="background" />
+                    
+                        
+                    <p>Title</p>
+                    <p>£100</p>
+                 </div>
+            )
+    }
+    
+  
+   
+   
+}
+
+export default itemBlock;
--- src/components/our-best-block/our-best-bloc.js
+++ src/components/our-best-block/our-best-bloc.js
@@ -1,5 +1,6 @@
 import './our-best-block.css' 
 import Paper from '../../images/paper.png'
+import {itemBlock }from './block'
 
 import {Component} from 'react'
 
@@ -10,24 +11,13 @@ class OurBestBlock extends Component {
 
         return (
             
-                <div className="our-best">
-                    <img  src={Paper} alt='paper'/>
-                    <h3>our Best</h3>
-                   
-                    <p className='p1' >
-                        Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-                        Afraid at highly months do things on at. Situation recommend objection do intention
-                        so questions. As greatly removed calling pleased improve an. Last ask him cold feel
-                        met spot shy want. Children me laughing we prospect answered followed. At it went
-                        is song that held help face.
-                    </p>
-                    <p className='p2'>
-                        Now residence dashwoods she excellent you. Shade being under his bed her, Much
-                        read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
-                        horrible but confined day end marriage. Eagerness furniture set preserved far
-                        recommend. Did even but nor are most gave hope. Secure active living depend sonrepair day ladies now.
-                    </p>
-                </div>
+                 <div className="wrapper">
+                    <img className="bg-image" src={Paper} alt="background" />
+                    <div className="centered-content">
+                        <h3>Our Best</h3>
+                        
+                    </div>
+                 </div>
             )
     }
     
